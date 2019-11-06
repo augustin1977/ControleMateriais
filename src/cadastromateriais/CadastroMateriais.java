edf17775f0efa2274c445c65a989836b82e55d76
Eric_Augustin <ericaugustin@hotmail.com>
2019-11-06 17:07:21 -0200

    Inclusão do metodo ultimoRegistro

--- src/cadastromateriais/CadastroMateriais.java
+++ src/cadastromateriais/CadastroMateriais.java
@@ -6,14 +6,14 @@ public class CadastroMateriais {
 
     public static void main(String[] args) throws IOException {
         Categorias c1=new Categorias();
+        Fornecedor f1=new Fornecedor();
+        Produtos p1=new Produtos();
+        f1.buscaRegistro(p1.getCodFornecedor());
         c1.buscaRegistro(3);
         System.out.println(c1.getcodCategoria()+" - "+c1.getnomeCategoria());
-        Fornecedor f1=new Fornecedor();
         f1.buscaRegistro(1);
         System.out.println(f1.getCodFornecedor()+" - "+f1.getNomeFornecedor()+" - "+f1.getTelefoneFornecedor());
-        Produtos p1=new Produtos();
         p1.buscaRegistro(5);
-        f1.buscaRegistro(p1.getCodFornecedor());
         System.out.println(p1.getCodProduto()+" - "+p1.getdescricao()+" - "+p1.getPreco()+" - "+p1.getCodCategoria()+" - "+p1.getCodFornecedor()+" - "+f1.getNomeFornecedor());
         Movimentacao m1= new Movimentacao();
         m1.buscaRegistro(10);
@@ -22,6 +22,12 @@ public class CadastroMateriais {
         f1.buscaRegistro(p1.getCodFornecedor());
         System.out.println(m1.getCodigoMovimentacao()+" - "+m1.getTipoMovimentacao()+" - "+m1.getQuantidadeMovimentacao()+" - "+m1.getCodProduto()+" - "+p1.getdescricao()+ " - R$"+ p1.getPreco()*m1.getQuantidadeMovimentacao()+" - Fornecido por: "+ f1.getNomeFornecedor()+" - categoria:"+ c1.getnomeCategoria());
         System.out.println(c1.UltimoCodigo());
+        System.out.println(f1.UltimoCodigo());
+        System.out.println(p1.UltimoCodigo());
+        System.out.println(m1.UltimoCodigo());
+        //c1.incluiRegistro(true);
+        //f1.incluiRegistro(true);
+        p1.incluiRegistro(true);
     }
 
 }
