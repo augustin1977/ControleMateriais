34032c24f19ae7a16cc222ade0427b2e785f7eee
Eric_Augustin <ericaugustin@hotmail.com>
2019-11-05 22:34:24 -0200

    Classe categoria implementada com busca mas sem inserção de dados

--- src/cadastromateriais/CadastroMateriais.java
+++ src/cadastromateriais/CadastroMateriais.java
@@ -1,8 +1,13 @@
 package cadastromateriais;
 
+import java.io.IOException;
+
 public class CadastroMateriais {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Categorias c1=new Categorias(1,"teste");
+        c1.buscaRegistro(2);
+        System.out.println(c1.getCod_categoria()+c1.getNome_categoria());
 
     }
 
