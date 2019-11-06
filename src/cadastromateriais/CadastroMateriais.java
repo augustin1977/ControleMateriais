058f7e6f16e327293a3fadf6895434cfcc3c5b9e
Eric_Augustin <ericaugustin@hotmail.com>
2019-11-06 14:09:28 -0200

    Classe movimentacao

--- src/cadastromateriais/CadastroMateriais.java
+++ src/cadastromateriais/CadastroMateriais.java
@@ -15,6 +15,11 @@ public class CadastroMateriais {
         p1.buscaRegistro(5);
         f1.buscaRegistro(p1.getCodFornecedor());
         System.out.println(p1.getCodProduto()+" - "+p1.getdescricao()+" - "+p1.getPreco()+" - "+p1.getCodCategoria()+" - "+p1.getCodFornecedor()+" - "+f1.getNomeFornecedor());
+        Movimentacao m1= new Movimentacao();
+        m1.buscaRegistro(10);
+        p1.buscaRegistro(m1.getCodProduto());
+        System.out.println(m1.getCodigoMovimentacao()+" - "+m1.getTipoMovimentacao()+" - "+m1.getQuantidadeMovimentacao()+" - "+m1.getCodProduto()+" - "+p1.getdescricao());
+        
 
     }
 
